a42e41ee4653f491221dd4b381f856fc47a89306
Admin <admin@Admins-MacBook-Pro.local>
2022-08-29 07:08:21 +0100

    clean code

--- src/App.js
+++ src/App.js
@@ -28,61 +28,6 @@ function App() {
   const [secondBal, setSecondBal] = useState();
 
 
-
-  // const reachFunctions = {
-  //   connect: async (secret, mnemonic = false) => {
-  //     let result = ""
-  //     try {
-  //       const account = mnemonic ? await reach.newAccountFromMnemonic(secret) : await reach.getDefaultAccount();
-  //       setAccount(account);
-  //       setView(views.DEPLOY_OR_ATTACH);
-  //       result = 'success';
-  //     } catch (error) {
-  //       result = 'failed';
-  //     }
-  //     return result;
-  //   },
-
-  //   setAsDeployer: (deployer = true) => {
-  //     if (deployer) {
-  //       setView(views.SET_TOKEN_INFO);
-  //     }
-  //     else {
-  //       setView(views.PASTE_CONTRACT_INFO);
-  //     }
-  //   },
-
-  //   deploy: async () => {
-  //     const contract = account.contract(backend);
-  //     backend.Deployer(contract, Deployer);
-  //     setView(views.DEPLOYING);
-  //     const ctcInfo = JSON.stringify(await contract.getInfo(), null, 2)
-  //     setContractInfo(ctcInfo);
-  //     setView(views.WAIT_FOR_ATTACHER)
-  //   },
-
-  //   attach: (contractInfo) => {
-  //     const contract = account.contract(backend, JSON.parse(contractInfo));
-  //     backend.Attacher(contract, Attacher)
-  //     setView(views.ATTACHING)
-  //   }
-  // }
-
-  //Participant Objects
-  // const Common = {
-  //   random: () => reach.hasRandom.random(),
-
-  //   test: () => setView(views.TEST_VIEW)
-  // }
-
-  // const Deployer = {
-  //   ...Common
-  // }
-
-  // const Attacher = {
-  //   ...Common
-  // }
-
   // Frontend Logic For Game ****************** //
 
   /** get wallet details */
